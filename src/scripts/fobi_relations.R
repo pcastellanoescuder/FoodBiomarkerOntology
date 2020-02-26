f62c5eff37604696820489a938ca8820c6c9c822
pol <polcaes@gmail.com>
2020-02-26 15:44:48 +0100

    new script to export CSV

--- src/scripts/fobi_relations.R
+++ src/scripts/fobi_relations.R
@@ -1,18 +1,18 @@
 library(tidyverse)
 library(data.table)
 
-fobi <- readr::read_csv("fobi-merged.csv")
-fobi <- fobi[!(is.na(fobi$`http://purl.obolibrary.org/obo/FOBI_00422`)) ,]
+fobi <- readr::read_csv2("/home/pol/Escritorio/FoodBiomarkerOntology/fobi-export.csv")
+# fobi <- fobi[!(is.na(fobi$`http://purl.obolibrary.org/obo/FOBI_00422`)) ,]
 
-fobi <- fobi[, c(4, 21)]
-colnames(fobi) <- c("food", "label")
+fobi <- fobi[!is.na(fobi$BiomarkerOf), ]
 
 fobi <- data.table(fobi)
 
-dt_out <- fobi[, list(metabolite = label,
-                      food = unlist(strsplit(food, '\t'))), 
+dt_out <- fobi[, list(metabolite = Entity,
+                      food = unlist(strsplit(BiomarkerOf, '\t'))), 
                by = 1:nrow(fobi)]
 dt_out$nrow <- NULL
 dt_out$metabolite <- gsub("'", '', dt_out$metabolite)
+dt_out$food <- gsub("'", "", dt_out$food, fixed = T)
 
-# write.csv(dt_out, "191202_fobi_relations", row.names = F)
+# write.csv(dt_out, "200226_fobi_relations", row.names = F)
